4f464b30305258bc708ba1be906fce7ca21129b7
Nicholas Swenson <dart200@gmail.com>
2021-10-01 22:20:36 +0800

    basic html template + bootstrap css

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask;
-app = Flask(__name__);
+import os
+from flask import Flask, render_template,send_from_directory
+
+app = Flask(__name__)
+
+def staticDir():
+    return os.path.join(app.root_path, 'static')
+
+@app.route('/favicon.png')
+def favicon():
+    return send_from_directory(staticDir(), 'favicon.png', mimetype='image/png')
 
 @app.route("/")
-def root():
-    return "Hello World!"
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route("/<short_id>")
+def view_data(short_id):
+    return "You are viewing short ID: {}".format(short_id)
