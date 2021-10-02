ee3a9491fc5029854f16829bf1f8705f76852bb1
Nicholas Swenson <dart200@gmail.com>
2021-10-02 22:58:35 +0800

    dockerized

--- app/app.py
+++ app/app.py
@@ -4,30 +4,31 @@ import random
 from flask import Flask,render_template,send_from_directory,request,redirect
 from flask_pymongo import PyMongo
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/tiny-url"
-mongo = PyMongo(app)
+application = Flask(__name__)
+mongo_login = None if not os.getenv('MONGODB_USERNAME') else os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@'
+application.config["MONGO_URI"] = 'mongodb://' + (mongo_login if mongo_login else '') + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
+mongo = PyMongo(application)
 
 def staticDir():
-    return os.path.join(app.root_path, 'static')
+    return os.path.join(application.root_path, 'static')
 
 def id_generator(size=8, chars=string.digits + string.ascii_lowercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
-@app.route('/favicon.png')
+@application.route('/favicon.png')
 def favicon():
     return send_from_directory(staticDir(), 'favicon.png', mimetype='image/png')
-@app.route('/main.js')
+@application.route('/main.js')
 def mainjs():
     return send_from_directory(staticDir(), 'main.js')
 
 
-@app.route("/")
-@app.route("/index")
+@application.route("/")
+@application.route("/index")
 def index():
     return render_template('index.html')
 
-@app.route("/shorten")
+@application.route("/shorten")
 def shorten():
     url = request.args.get('url')
     if (not url):
@@ -40,7 +41,12 @@ def shorten():
             return render_template('index.html', url=request.url_root+slug)
 
 
-@app.route("/<short_id>")
+@application.route("/<short_id>")
 def view_data(short_id):
     doc = mongo.db.url.find_one_or_404({"slug": short_id})
     return redirect('http://'+doc['url'], code=302)
+
+if __name__ == "__main__":
+    ENVIRONMENT_DEBUG = os.getenv("APP_DEBUG", True)
+    ENVIRONMENT_PORT = os.getenv("APP_PORT", 5000)
+    application.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
